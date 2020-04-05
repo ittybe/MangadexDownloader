61c77ed630667fa668691a88db5871d8c604dc05
ittybe <misha.kan7.2004.20@gmail.com>
2020-04-05 15:45:28 +0300

    add IMangaParser interface

---
+++ MangadexDownloader/MangadexDownloader/ContentCollecting/IContentCollector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//PdfImage image = PdfImage.FromFile(@"D:\images\bear.tif");
+////Set image display location and size in PDF
+
+//float widthFitRate = image.PhysicalDimension.Width / page.Canvas.ClientSize.Width;
+
+//float heightFitRate = image.PhysicalDimension.Height / page.Canvas.ClientSize.Height;
+
+//float fitRate = Math.Max(widthFitRate, heightFitRate);
+
+//float fitWidth = image.PhysicalDimension.Width / fitRate;
+
+//float fitHeight = image.PhysicalDimension.Height / fitRate;
+
+//page.Canvas.DrawImage(image, 30, 30, fitWidth, fitHeight);
+namespace MangadexDownloader.ContentCollecting
+{
+    interface IContentCollector
+    {
+
+    }
+}
---
+++ MangadexDownloader/MangadexDownloader/Parsing/ContentParsing/IMangaParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using MangadexDownloader.ContentInfo;
+
+namespace MangadexDownloader.Parsing.ContentParsing
+{
+    /// <summary>
+    /// interface for MangaParser
+    /// </summary>
+    public interface IMangaParser
+    {
+        /// <summary>
+        /// information about manga
+        /// </summary>
+        IMangaInfo MangaInfo { get; }
+        /// <summary>
+        /// Contains information about all Chapters
+        /// </summary>
+        List<IChapterInfo> ChaptersInfo { get; }
+
+        /// <summary>
+        /// dir for pages
+        /// </summary>
+        DirectoryInfo Dir { get; set; }
+
+        /// <summary>
+        /// parse all pages
+        /// </summary>
+        /// <param name="match">parse if only ChapterInfo is match,</param>
+        void Parse(Predicate<IChapterInfo> match);
+     
+        /// <summary>
+        /// parse chapters info into List IChapterInfo property
+        /// </summary>
+        /// <param name="match">add ChapterInfo to list if ChapterInfo match</param>
+        void ParseChaptersInfo(Predicate<IChapterInfo> match);
+    }
+}
