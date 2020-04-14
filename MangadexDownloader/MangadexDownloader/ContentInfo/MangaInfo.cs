d2e5515ec58e76cd2bf4332d9fa3b9c0eae3336c
ittybe <misha.kan7.2004.20@gmail.com>
2020-04-14 12:28:45 +0300

    fix little fix and add new file

--- MangadexDownloader/MangadexDownloader/ContentInfo/MangaInfo.cs
+++ MangadexDownloader/MangadexDownloader/ContentInfo/MangaInfo.cs
@@ -6,6 +6,7 @@ using System.Text;
 
 namespace MangadexDownloader.ContentInfo
 {
+    [JsonConverter(typeof(JsonPathConverter))]
     public class MangaInfo : IMangaInfo
     {
         /// <summary>
